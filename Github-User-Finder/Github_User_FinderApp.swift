fbc67b1112447831b916fa182fdf45e319fd9415
hamzaahmedsiddiqui <dev.hamxa@gmail.com>
2024-11-29 02:33:04 +0100

    Initial commit: Set up project

---
+++ Github-User-Finder/Github_User_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_User_FinderApp.swift
+//  Github-User-Finder
+//
+//  Created by hamza Ahmed on 2024-11-29.
+//
+
+import SwiftUI
+
+@main
+struct Github_User_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
